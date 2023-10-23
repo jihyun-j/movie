2c5b231fca7e2c28eb01097c6cde923bd7709db4
jihyun-j <jihyun9142@gmail.com>
2023-10-23 17:35:04 +0900

    Final Version

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const printMovieData = (movie) => {
     const cardContainer = document.createElement("div");
     cardContainer.classList.add("card-container");
 
-    // 카드 안에 들어가는 정보들
+    // 카드 안에 들어가는 영화 정보들
     cardContainer.innerHTML = `
         <img class="poster_img" src="https://image.tmdb.org/t/p/w500${img}" alt="" />
         <div class="movie-info">
@@ -45,72 +45,77 @@ const printMovieData = (movie) => {
         </div>
       `;
 
-    // 모든 영화가 들어가는 컨테이너 안에 영화 넣기
+    // 메인 영화 컨테이너 안에 카드 넣기
     movieContainerEl.append(cardContainer);
 
-    // alert 팝업 기능
+    // 영화 ID값 alert 띄우기
     cardContainer.addEventListener("click", function () {
       alert(`이 영화의 ID 넘버는 ${id}`);
     });
   });
 };
 
+// 영화 검색 기능 함수
 const searchMovie = (e) => {
   e.preventDefault();
-  const movieTitles = document.querySelectorAll(".title");
-  let searchInput = searchInputEl.value;
-  const cards = document.querySelectorAll(".card-container");
-  const count = document.querySelector(".count-movies");
+  const movieTitles = document.querySelectorAll(".title"); // 모든 영화 제목 가져오기
+  const cards = document.querySelectorAll(".card-container"); // 모든 카드 가져오기
+  let searchInput = searchInputEl.value; // 검색 input 값 가져오기
+  const count = document.querySelector(".count-movies"); // 검색된 영화 개수 넣을 자리 가져오기
 
-  let movieCount = 0;
+  let movieCount = 0; // 검색 결과 초기화
 
   movieTitles.forEach((title, index) => {
+    // search input 값이 영화 제목에 포함되어 있는 경우 카드 보이기
     let isVisible =
       title.innerHTML.toUpperCase().includes(searchInput.toUpperCase()) ||
       title.innerHTML.toLowerCase().includes(searchInput.toLowerCase());
 
     if (isVisible) {
+      // 카드 하나 하나씩 순회하면서 만약 영화제목에 input 값이 포함 되어 있으면 카드 보이기
       cards[index].classList.remove("hide");
-      movieCount++;
+      movieCount++; // 검색된 결과 더하기
     } else {
+      // 만약 영화제목에 input 값이 포함 되어 있지 않으면 카드 숨기기
       cards[index].classList.add("hide");
     }
   });
-  count.innerText = `검색된 영화: ${movieCount}`;
+  count.innerText = `검색된 영화: ${movieCount}`; // 검색된 영화 개수 보이기
 };
 
+// 영화 검색 form에 대한 이벤트
 searchFormEl.addEventListener("submit", searchMovie);
 
 // Slide
-
 const slide = (movies) => {
-  let slideCotainer = document.querySelector(".slide-container");
+  let slideCotainer = document.querySelector(".slide-container"); // 메인 슬라이드 컨테이너 가져오기
 
+  // 5초마다 바뀌는 영화 정보
   setInterval(() => {
-    let random = Math.floor(Math.random() * movies.length);
-    let backdropImgPath = `https://image.tmdb.org/t/p/w500${movies[random].backdrop_path}`;
-    let movieTitle = movies[random].title;
-    let movieOverview = movies[random].overview.substring(0, 200) + "...";
+    let random = Math.floor(Math.random() * movies.length); // 랜덤 인덱스
+    let backdropImgPath = `https://image.tmdb.org/t/p/w500${movies[random].backdrop_path}`; // backdrop 이미지 랜덤으로 가져오기
+    let movieTitle = movies[random].title; // 영화 제목 랜덤으로 가져오기
+    let movieOverview = movies[random].overview.substring(0, 200) + "..."; // 영화 줄거리도 가져오기
 
+    // 메인 슬라이드에 랜덤 이미지와 그라데이션과 주기
     slideCotainer.style.backgroundImage = `linear-gradient(to left, rgba(255,255,255,0), rgba(0,0,0,1)), url(${backdropImgPath})`;
 
-    let slideTemp = `
+    // 슬라이드 이미지 위에 들어갈 정보들
+    let slideTemp = ` 
     <div class="movie-info-container">
       <p class="movie-title">${movieTitle}</p>
       <p class="movie-overview">${movieOverview}</p>
-      <button class="learn-more">Learn More</button>
+      <button class="play-now-btn">Play now</button>
     </div>
     `;
 
+    // 메인 슬라이드에 영화 정보 추가하기
     slideCotainer.innerHTML = slideTemp;
   }, 5000);
 };
 
-slide();
-
-// header background
+// 스크롤 했을 때, Header 배경색 바꾸기
 const hearderEl = document.querySelector(".header-container");
-
 window.addEventListener("scroll", function () {
   if (window.scrollY > 300) {
     hearderEl.classList.add("add-background");
